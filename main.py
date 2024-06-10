ad9db08b952146d7b0547ad63ec6e7d45b4a046c
Tatranka <113363195+Tatrank@users.noreply.github.com>
2024-06-10 13:14:37 +0200

    better version lol

--- main.py
+++ main.py
@@ -391,4 +391,4 @@ def userAPI(id):
 if __name__ == '__main__':
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
